54f56c231c1b6e42e66499fe0d3d06c37563afc0
FcoJavierVG <145746999+FcoJavierVGizv@users.noreply.github.com>
2025-05-30 17:57:45 +0200

    Delete api-flask directory

--- api-flask/models.py
+++
@@ -1,9 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-db = SQLAlchemy()
-
-class Consulta(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    tipo = db.Column(db.String(50), nullable=False)
-    parametro = db.Column(db.String(100))
-    fecha_hora = db.Column(db.DateTime, nullable=False)
-    ip_usuario = db.Column(db.String(100), nullable=False)
